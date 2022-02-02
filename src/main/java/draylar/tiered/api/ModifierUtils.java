44a07f7ccbdf91f7c18719eabf169ec40c705e68
Stereowalker <35645830+Stereowalker@users.noreply.github.com>
2022-02-02 22:56:18 +0100

    Ported the weighted system from 1.18

--- src/main/java/draylar/tiered/api/ModifierUtils.java
+++ src/main/java/draylar/tiered/api/ModifierUtils.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import org.apache.commons.lang3.mutable.MutableInt;
+
 import draylar.tiered.Tiered;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
@@ -19,17 +21,37 @@ public class ModifierUtils {
      * @return  id of random attribute for item in {@link ResourceLocation} form, or null if there are no valid options
      */
     public static ResourceLocation getRandomAttributeIDFor(Item item) {
-        List<ResourceLocation> potentialAttributes = new ArrayList<>();
+        List<ResourceLocation> potentialAttributes = new ArrayList<>();		
+        List<ResourceLocation> chosenAttribute = new ArrayList<>();
 
+        MutableInt totalWeight = new MutableInt();
+        
         // collect all valid attributes for the given item
         Tiered.ATTRIBUTE_DATA_LOADER.getItemAttributes().forEach((id, attribute) -> {
             if(attribute.isValid(Registry.ITEM.getKey(item))) {
                 potentialAttributes.add(new ResourceLocation(attribute.getID()));
+                totalWeight.add(attribute.getWeight());
             }
         });
+        
+        if (totalWeight.getValue() > 0) {
+			MutableInt randomAttribute = new MutableInt(new Random().nextInt(totalWeight.getValue())+1);
+			MutableInt i = new MutableInt();
+			Tiered.ATTRIBUTE_DATA_LOADER.getItemAttributes().forEach((id, attribute) -> {
+				if(attribute.isValid(Registry.ITEM.getKey(item))) {
+					i.add(attribute.getWeight());
+					if (i.getValue() >= randomAttribute.getValue()) {
+						chosenAttribute.add(id);
+					}
+				}
+			});
+		}
 
-        // return a random attribute if there are any, or null if there are none
-        if(potentialAttributes.size() > 0) {
+        if(chosenAttribute.size() > 0) {
+			return chosenAttribute.get(0);
+		}
+		// return a random attribute if there are any, or null if there are none
+		else if(potentialAttributes.size() > 0) {
             return potentialAttributes.get(new Random().nextInt(potentialAttributes.size()));
         } else {
             return null;
