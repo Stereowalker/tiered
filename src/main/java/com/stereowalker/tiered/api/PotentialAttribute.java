736824ee521026d3fb75bebddc93dbe2c52fd1eb
Stereowalker <stereowalker.mc@gmail.com>
2023-05-30 14:05:19 +0100

    Certain tags and items can now be excluded using "exclusions"

--- src/main/java/com/stereowalker/tiered/api/PotentialAttribute.java
+++ src/main/java/com/stereowalker/tiered/api/PotentialAttribute.java
@@ -9,66 +9,71 @@ import net.minecraft.resources.ResourceLocation;
 
 public class PotentialAttribute implements WeightedObject {
 
-    private final String id;
-    private final int weight;
-    
-    private final int reforge_durability_cost;
-    private final int reforge_experience_cost;
-    private final String reforge_item;
-    private final List<ItemVerifier> verifiers;
-    private final Style style;
-    private final List<AttributeTemplate> attributes;
+	private final String id;
+	private final int weight;
 
-    public PotentialAttribute(String id, int weight, int reforge_durability_cost, int reforge_experience_cost, String reforge_item, List<ItemVerifier> verifiers, Style style, List<AttributeTemplate> attributes) {
-        this.id = id;
-        this.weight = weight;
-        this.reforge_durability_cost = reforge_durability_cost;
-        this.reforge_experience_cost = reforge_experience_cost;
-        this.reforge_item = reforge_item;
-        this.verifiers = verifiers;
-        this.style = style;
-        this.attributes = attributes;
-    }
-    
-    public String getID() {
-        return id;
-    }
-    
-    public int getWeight() {
+	private final int reforge_durability_cost;
+	private final int reforge_experience_cost;
+	private final String reforge_item;
+	private final List<ItemVerifier> verifiers;
+	private final List<ItemVerifier> exclusions;
+	private final Style style;
+	private final List<AttributeTemplate> attributes;
+
+	public PotentialAttribute(String id, int weight, int reforge_durability_cost, int reforge_experience_cost, String reforge_item, List<ItemVerifier> verifiers, List<ItemVerifier> exclusions, Style style, List<AttributeTemplate> attributes) {
+		this.id = id;
+		this.weight = weight;
+		this.reforge_durability_cost = reforge_durability_cost;
+		this.reforge_experience_cost = reforge_experience_cost;
+		this.reforge_item = reforge_item;
+		this.verifiers = verifiers;
+		this.exclusions = exclusions;
+		this.style = style;
+		this.attributes = attributes;
+	}
+
+	public String getID() {
+		return id;
+	}
+
+	public int getWeight() {
 		return weight;
 	}
-    
-    public int getReforgeDurabilityCost() {
+
+	public int getReforgeDurabilityCost() {
 		return Math.max(reforge_durability_cost, 1);
 	}
-    
-    public int getReforgeExperienceCost() {
+
+	public int getReforgeExperienceCost() {
 		return Math.max(reforge_experience_cost, 1);
 	}
-    
-    public String getReforgeItem() {
+
+	public String getReforgeItem() {
 		return reforge_item;
 	}
 
-    public List<ItemVerifier> getVerifiers() {
-        return verifiers;
-    }
+	public List<ItemVerifier> getVerifiers() {
+		return verifiers;
+	}
 
-    public boolean isValid(ResourceLocation id) {
-        for(ItemVerifier verifier : verifiers) {
-            if(verifier.isValid(id)) {
-                return true;
-            }
-        }
+	public List<ItemVerifier> getExclusions() {
+		return exclusions;
+	}
 
-        return false;
-    }
+	public boolean isValid(ResourceLocation id) {
+		if (exclusions != null)
+			for(ItemVerifier exclusion : exclusions)
+				if(exclusion.isValid(id)) return false;
+		for(ItemVerifier verifier : verifiers)
+			if(verifier.isValid(id)) return true;
+		return false;
+	}
 
-    public Style getStyle() {
-        return style;
-    }
+	public Style getStyle() {
+		return style;
+	}
 
-    public List<AttributeTemplate> getAttributes() {
-        return attributes;
-    }
+	public List<AttributeTemplate> getAttributes() {
+		return attributes;
+	}
 }
