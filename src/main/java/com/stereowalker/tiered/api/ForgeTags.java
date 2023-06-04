9a47f6db552322a1add470610d7781e6e17a9eff
Stereowalker <stereowalker.mc@gmail.com>
2023-06-04 14:29:16 +0100

    Used forge provided tags for helmets. chestplates, leggings, boots, swords, axes, pickaxes, shovels, hoes and bows

--- src/main/java/com/stereowalker/tiered/api/ForgeTags.java
+++ src/main/java/com/stereowalker/tiered/api/ForgeTags.java
@@ -9,18 +9,8 @@ import net.minecraft.world.item.Item;
 public class ForgeTags {
 
 	//Armor
-    public static final TagKey<Item> HELMETS = register("helmets");
-    public static final TagKey<Item> CHESTPLATES = register("chestplates");
-    public static final TagKey<Item> LEGGINGS = register("leggings");
-    public static final TagKey<Item> BOOTS = register("boots");
     public static final TagKey<Item> SHIELDS = register("shields");
     //Tools
-    public static final TagKey<Item> SWORD = register("swords");
-    public static final TagKey<Item> AXE = register("axes");
-    public static final TagKey<Item> HOE = register("hoes");
-    public static final TagKey<Item> SHOVEL = register("shovels");
-    public static final TagKey<Item> PICKAXE = register("pickaxes");
-    public static final TagKey<Item> BOWS = register("bows");
 
     private ForgeTags() { }
 
