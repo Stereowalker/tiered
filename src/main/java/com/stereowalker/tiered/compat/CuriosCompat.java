baf66b75398c5bbbef160e4bed6b909bd818f3d1
Stereowalker <stereowalker.mc@gmail.com>
2024-07-12 14:53:45 +0100

    disabled curios compatibility temporarily

--- src/main/java/com/stereowalker/tiered/compat/CuriosCompat.java
+++ src/main/java/com/stereowalker/tiered/compat/CuriosCompat.java
@@ -10,18 +10,18 @@ import net.minecraftforge.eventbus.api.Event;
 
 public class CuriosCompat {
 	public static void load() {
-		Supplier<Consumer<? extends Event>> i = () -> 
-		(Consumer<top.theillusivec4.curios.api.event.CurioAttributeModifierEvent>)(event)->{
-			Tiered.AppendAttributesToOriginal(
-					event.getItemStack(), 
-					event.getSlotContext().identifier(), 
-					Tiered.isPreferredCurioSlot(event.getItemStack(), event.getSlotContext().identifier()), 
-					"CurioAttributeModifiers"/* , event.getModifiers() */,
-					template -> template.getRequiredCurioSlot(), 
-					template -> template.getOptionalCurioSlot(), 
-					(template) -> template.realize(event::addModifier, event.getSlotContext().identifier()));
-		}
-		;
-		MinecraftForge.EVENT_BUS.addListener(i.get());
+//		Supplier<Consumer<? extends Event>> i = () -> 
+//		(Consumer<top.theillusivec4.curios.api.event.CurioAttributeModifierEvent>)(event)->{
+//			Tiered.AppendAttributesToOriginal(
+//					event.getItemStack(), 
+//					event.getSlotContext().identifier(), 
+//					Tiered.isPreferredCurioSlot(event.getItemStack(), event.getSlotContext().identifier()), 
+//					"CurioAttributeModifiers"/* , event.getModifiers() */,
+//					template -> template.getRequiredCurioSlot(), 
+//					template -> template.getOptionalCurioSlot(), 
+//					(template) -> template.realize(event::addModifier, event.getSlotContext().identifier()));
+//		}
+//		;
+//		MinecraftForge.EVENT_BUS.addListener(i.get());
 	}
 }
