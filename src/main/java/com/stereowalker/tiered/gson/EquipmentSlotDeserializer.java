98316c8dffbb78dfd11b790e59ffe49d0457125c
Stereowalker <stereowalker.mc@gmail.com>
2024-07-12 14:48:15 +0100

    Used equipment slot groups instead of just slots for tiered

--- src/main/java/com/stereowalker/tiered/gson/EquipmentSlotDeserializer.java
+++ src/main/java/com/stereowalker/tiered/gson/EquipmentSlotDeserializer.java
@@ -7,13 +7,13 @@ import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
 
-import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.EquipmentSlotGroup;
 
 
-public class EquipmentSlotDeserializer implements JsonDeserializer<EquipmentSlot> {
+public class EquipmentSlotDeserializer implements JsonDeserializer<EquipmentSlotGroup> {
 
     @Override
-    public EquipmentSlot deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-        return EquipmentSlot.byName(json.getAsString().toLowerCase());
+    public EquipmentSlotGroup deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+    	return EquipmentSlotGroup.valueOf(json.getAsString().toUpperCase());
     }
 }
