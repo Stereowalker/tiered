d6b8a04fda0cbd587b22aed2a403410a75d3229c
Stereowalker <stereowalker.mc@gmail.com>
2024-08-06 03:22:50 +0100

    Updated UnionLib

--- src/main/java/com/stereowalker/tiered/Reforged.java
+++ src/main/java/com/stereowalker/tiered/Reforged.java
@@ -43,6 +43,7 @@ import com.stereowalker.unionlib.world.item.AccessoryItem;
 
 import net.minecraft.Util;
 import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.tags.TagKey;
@@ -188,8 +189,8 @@ public class Reforged extends MinecraftMod implements PacketHolder {
 
 	@Override
 	public void setupRegistries(RegistryCollector collector) {
-		collector.addRegistryHolder(ComponentsRegistry.class);
-		collector.addRegistryHolder(ItemRegistries.class);
+		collector.addRegistryHolder(Registries.DATA_COMPONENT_TYPE, ComponentsRegistry.class);
+		collector.addRegistryHolder(Registries.ITEM, ItemRegistries.class);
 	}
 
 	@Override
@@ -202,7 +203,7 @@ public class Reforged extends MinecraftMod implements PacketHolder {
 	}
 
 
-	@RegistryHolder(namespace = "tiered", registry = DataComponentType.class)
+	@RegistryHolder(namespace = "tiered")
 	public class ComponentsRegistry {
 		@RegistryObject("tiered_modifier")
 		public static final DataComponentType<ResourceLocation> MODIFIER = register(
@@ -214,7 +215,7 @@ public class Reforged extends MinecraftMod implements PacketHolder {
 		}
 	}
 
-	@RegistryHolder(registry = Item.class, namespace = "tiered")
+	@RegistryHolder(namespace = "tiered")
 	public class ItemRegistries {
 		@RegistryObject("armorers_hammer")
 		public static final Item ARMORERS_HAMMER = new Item(new Item.Properties().durability(20));
